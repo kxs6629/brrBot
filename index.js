ccee69e898faba1906e3d917b7ad54d9b824aac4
Kenny S <35939549+kxs6629@users.noreply.github.com>
2021-04-01 17:50:44 -0400

    More index.js code created

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ const client = new Discord.Client();
 const prefix = "!";
 const commandList = ["brr"];
 
+client.login(config.BOT_TOKEN);
+
 client.on("ready",() => {
 	console.log("Ready!");
 	const channel = client.channels.cache.get('762848205054148619');
 	channel.send('uh oh');
 	client.user.setStatus("xd");
 })
+
+client.on('message', async message => {
+    if(!message.guild) return;
+    if(message.content === '!join'){
+        if(message.member.voice.channel){
+            const connection = await message.member.voice.channel.join();
+        }
+        else{
+            message.reply('join a voice channel nerd');
+        }
+    }
+});
+
