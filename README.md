56e313187f56943249157b3d97a2fa30b71d9415
Kenny S <35939549+kxs6629@users.noreply.github.com>
2021-04-05 03:08:31 -0400

    Updated README to include new goals Updated index.js to add more functionality to brr

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # brrBot
 Funny bot that makes funny noise
 
-## Purpose
-Literally just plays one sound file and leaves when summoned
-
 ## Author
 Ken Scott
+
+## Purpose
+Originally this was going to be a gag bot that would play a fart sound and leave. It's now evolving into a bot that can play youtube audio in calls as well as the original sound :^)
+
+## ToDo
+* add pause and resume functionality for youtube video playback
--- index.js
+++ index.js
@@ -4,30 +4,80 @@ const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 
 const prefix = "!";
-const commandList = ["brr"];
+const commandList = ["brr","play","stop"];
+const description = ["play the funny sound","play a youtube link by doing !play linkhere","gamer time, stop all sounds and make brr leave"];
+
+let helpString='';
+helpString.concat('List of commands: \n');
+for(var i =0; i < commandList.length; i++){
+    helpString+= "!"+commandList[i] +" : "+description[i]+'\n';
+}
 
 client.login(config.BOT_TOKEN);
-
 client.on("ready",() => {
 	console.log("Ready!");
-	const channel = client.channels.cache.get('546437977715376128');
+	//Gonna fix this later since it's 3AM but sends a message to server so we know it works
+    const channel = client.channels.cache.get('546437977715376128');
+    // const channel = client.channels.cache.get('762848205054148619');
 	channel.send('uh oh');
-	client.user.setStatus("xd");
+	client.user.setPresence({
+        status: "online",
+        activity: {
+            name: "chat for the next command !help",
+            type: "LISTENING"
+        }
+    });
 })
 
 client.on('message', async message => {
-    if(!message.guild) return;
-    if(message.content === '!join'){
-        if(message.member.voice.channel){
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = await connection.play(require("path").join(__dirname, '/media/brr.mp3'));
-            dispatcher.on('finish', () =>{
-                message.member.voice.channel.leave();
-            })
+    if(message.author.bot) return;
+    else if(message.content.startsWith(prefix)){
+        const commandBody = message.content.slice(prefix.length);
+	    const args = commandBody.split(' ');
+	    const command = args.shift().toLowerCase();
+
+        switch(command){
+            case 'help':
+                message.reply("ugh fine i guess i'll help...here: \n"+helpString);
+                break;
+            case 'brr':
+                if(message.member.voice.channel){
+                    const connection = await message.member.voice.channel.join();
+                    const dispatcher = await connection.play(require("path").join(__dirname, '/media/brr.mp3'));
+                    dispatcher.on('finish', () =>{
+                        message.member.voice.channel.leave();
+                    })
+                }
+                else{
+                    message.reply('join a voice channel nerd');
+                }
+                break;
+            case 'play':
+                if(message.member.voice.channel){
+                    const connection = await message.member.voice.channel.join();
+                    const dispatcher = await connection.play(ytdl(args.shift(), {filter: 'audioonly'}));
+                    dispatcher.on('finish',() =>{
+                        message.member.voice.channel.leave();
+                    })
+                }
+                else{
+                    message.reply('join a voice channel nerd');
+                }
+                break;
+            case 'stop':
+                if(message.member.voice.channel){
+                    message.member.voice.channel.leave();
+                }
+                else{
+                    message.reply('join a voice channel nerd');
+                }
+                break;
+            default:
+                message.reply('its not like i want to help you or anything. use !help for a list of commands');
+
         }
-        else{
-            message.reply('join a voice channel nerd');
-        }
+
     }
+    
 });
 
